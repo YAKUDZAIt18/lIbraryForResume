0dd032cfa7e9c1c400049f177935ac0b9af347bf
YAKUZA <sir.quk2014@yandex.ru>
2021-03-26 12:48:36 +0300

    Query

--- lIbraryForResume/Class1.cs
+++ lIbraryForResume/Class1.cs
@@ -6,6 +6,11 @@ namespace lIbraryForResume
     {
         const double PI = 3.14;
 
-        public static 
+        public static double Square(double r) => PI * Math.Pow(r, 2);
+        public static double Square(double a,double b,double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
     }
 }
